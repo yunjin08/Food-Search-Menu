58f88336a50cc591a586afb145aa710ca1bc8ff3
Yunjin <jed.donaire08@gmail.com>
2023-12-24 20:45:37 +0800

    Added MealDetails Routing

---
+++ src/views/MealDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <pre>{{ meal }}</pre>
+  </div>
+</template>
+
+<script setup>
+import axiosClient from "../axiosClient";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import axiosClient from "../axiosClient";
+
+const route = useRoute();
+const meal = ref({});
+
+onMounted(() => {
+  axiosClient.get(`lookup.php?i=${route.params.id}`).then(({ data }) => {
+    debugger;
+    meal.value = data;
+  });
+});
+</script>
