3b0acc14e1a952e1f261e62f82b69318f58375a2
Yunjin <jed.donaire08@gmail.com>
2023-12-24 23:23:00 +0800

    Make the meals into component

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -10,37 +10,14 @@
   </div>
 
   <div class="grid grid-cols-1 md:grid-cols-3 gap-8 p-8 px-24">
-    <div
-      v-for="meal of meals"
-      :key="meal.idMeal"
-      class="bg-white shadow rounded-xl"
-    >
-      <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
-        <img
-          :src="meal.strMealThumb"
-          :alt="meal.strMeal"
-          class="rounded-t-xl h-64 w-full object-cover"
-        />
-      </router-link>
-      <div class="p-3">
-        <h3 class="bold">{{ meal.strMeal }}</h3>
-        <p>S</p>
-        <div class="flex items-center justify-between">
-          <a
-            :href="meal.strYoutube"
-            target="_blank"
-            class="px-3 py-2 rounded border-2 border-red-500 bg-red-500 hover:bg-red-600 text-white transition-colors"
-            >Youtube</a
-          >
-        </div>
-      </div>
-    </div>
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
 </template>
 
 <script setup>
 import { ref, computed, onMounted } from "vue";
 import store from "../store";
+import MealItem from "../components/MealItem.vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
