cbb6c0f78307f52e2e2e8ec3e10ca53b8986045e
Yunjin <jed.donaire08@gmail.com>
2023-12-25 23:47:24 +0800

    Added Search Function for Ingredients

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <div class="p-8 pb-0 px-24">
+      <input
+        v-model="keyword"
+        type="text"
+        class="rounded border-gray-200 w-full border-2 mb-3"
+        placeholder="Search for Ingredients"
+      />
+    </div>
     <div class="p-8">
       <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
       <router-link
@@ -7,7 +15,7 @@
           name: 'byIngredients',
           params: { ingredients: ingredient.strIngredient },
         }"
-        v-for="ingredient of ingredients"
+        v-for="ingredient of computedIngredients"
         :key="ingredient.idIngredient"
         class="block bg-white rounded p-3 mb-3 shadow"
       >
@@ -21,10 +29,24 @@
 </template>
 
 <script setup>
+import { computed } from "@vue/reactivity";
 import { onMounted, ref } from "vue";
 import axiosClient from "../axiosClient";
 
+const keyword = ref("");
 const ingredients = ref([]);
+
+const computedIngredients = computed(() => {
+  if (!computedIngredients) return ingredients;
+  return ingredients.value.filter((i) => {
+    return (
+      (i.strDescription || "")
+        .toLowerCase()
+        .includes(keyword.value.toLowerCase()) ||
+      i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase())
+    );
+  });
+});
 
 onMounted(() => {
   axiosClient.get("list.php?i=list").then(({ data }) => {
