ac5e676abc4477989b336303ed2013ebf253a301
Yunjin <jed.donaire08@gmail.com>
2023-12-25 22:24:21 +0800

    Added meals by ingredients

--- src/views/MealsByIngredients.vue
+++ src/views/MealsByIngredients.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="text-black"><p>By Ingredient</p></div>
+  <div>
+    <div class="p-8">
+      <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+      <div
+        v-for="ingredient of ingredients"
+        :key="ingredient.id"
+        class="bg-white rounded p-3 mb-3 shadow"
+      >
+        <h3 class="text-2xl font-bold">
+          {{ ingredient.strIngredient }}
+        </h3>
+        <p>{{ ingredient.strDescription }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import axiosClient from "../axiosClient";
+
+const ingredients = ref([]);
+
+onMounted(() => {
+  axiosClient.get("list.php?i=list").then(({ data }) => {
+    ingredients.value = data.meals;
+  });
+});
+</script>
--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -10,22 +10,27 @@
   </div>
 
   <div>
-    <pre>
-      {{ meals }}
-    </pre>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8 p-8 px-24">
+      <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { computed, onMounted } from "vue";
+import { computed, onMounted, watch } from "vue";
 import store from "../store";
 import { useRoute } from "vue-router";
+import MealItem from "../components/MealItem.vue";
 
 const route = useRoute();
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-const meals = computed(() => store.state.mealsByLetter);
+const meals = computed(() => store.state.mealsByLetter.data);
 
 onMounted(() => {
+  store.dispatch("searchMealsByLetter", route.params.letter);
+});
+
+watch(route, () => {
   store.dispatch("searchMealsByLetter", route.params.letter);
 });
 </script>
