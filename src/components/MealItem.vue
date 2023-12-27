8c803216a5dea48d899ed2e30ab5175c8a1e747f
Yunjin <jed.donaire08@gmail.com>
2023-12-27 21:08:15 +0800

    Added MealItem Tags

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white shadow rounded-xl">
+  <div class="bg-white mb-10 shadow rounded-xl">
     <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
       <img
         :src="meal.strMealThumb"
@@ -7,18 +7,22 @@
         class="rounded-t-xl h-64 w-full object-cover"
       />
     </router-link>
-    <div class="p-3">
+    <div class="p-3 px-4">
       <h3 class="font-semibold font-serif mt-2 text-xl text-[#5d582a]">
         {{ meal.strMeal }}
       </h3>
-      <p class="text-sm text-[#7d784a]">{{ meal.strArea }}</p>
-      <p class="text-sm text-[#7d784a]">{{ meal.strCategory }}</p>
-      <div class="flex items-center justify-between">
+      <p class="text-md mt-1 mb-1 text-[#5d582a] font-medium">
+        Area: {{ meal.strArea }}
+      </p>
+      <p class="text-md text-[#5d582a] font-medium">
+        Category: {{ meal.strCategory }}
+      </p>
+      <div class="flex items-center justify-between mt-4 mb-2">
         <a
           :href="meal.strYoutube"
           target="_blank"
-          class="px-3 py-2 rounded border-2 border-red-500 bg-red-500 hover:bg-red-600 text-white transition-colors"
-          >Youtube</a
+          class="px-6 py-2 rounded border-2 border-red-500 bg-red-500 hover:bg-red-600 text-white transition-colors"
+          >Watch on Youtube</a
         >
       </div>
     </div>
