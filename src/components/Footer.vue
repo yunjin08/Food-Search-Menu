be926a681233eab786685c47006d9e39e3975003
Yunjin <jed.donaire08@gmail.com>
2023-12-27 15:16:00 +0800

    Added Footer Text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,26 @@
 <template>
   <header
-    class="bg-white shadow-md h-32 flex items-center justify-center font-serif text-xl"
+    class="bg-white flex-col border-t-2 shadow border-[#e8e8d2] h-36 flex items-center justify-center font-serif text-xl"
   >
     <img
       src="../images/bg3.webp"
-      class="object-cover absolute w-[100vw] h-32 left-0 opacity-[0.3] pointer-events-none"
+      class="object-cover absolute w-[100vw] h-36 left-0 opacity-[0.2] pointer-events-none"
       alt="bg-3"
     />
+    <div class="z-10 mt-2">
+      <div class="flex flex-col text-[#a7a16a]">
+        <h1 class="font-bold">Jed Edison Donaire</h1>
+        <h2>University of the Philippines</h2>
+        <p class="font-bold">
+          Contact me <span class="font-normal"> | 966 278 1586 </span>
+        </p>
+      </div>
+      <div></div>
+    </div>
+    <p
+      class="text-sm font-bold w-full flex items-center justify-center mt-5 text-[#a7a16a]"
+    >
+      &#169; 2023. Jed Edison Donaire
+    </p>
   </header>
 </template>
