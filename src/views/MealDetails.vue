c03aac03464e509458828f8deb9394ae4aba58a5
Yunjin <jed.donaire08@gmail.com>
2023-12-30 13:20:26 +0800

    style Mealdetails

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,50 +1,63 @@
 <template>
-  <div class="flex flex-col items-center justify-center max-w-[50%] mx-auto">
-    <h1 class="text-8xl calligraphy-paragraph font-bold text-[#5d582a] p-8">
-      {{ meal.strMeal }}
-    </h1>
-    <img :src="meal.strMealThumb" :alt="meal.strMeal" class="w-[30rem]" />
-    <div
-      class="grid text-[#5d582a] grid-cols-1 mt-2 sm:grid-cols-3 text-lg py-2 justify-evenly text-center"
-    >
-      <div>
-        <strong class="font-bold text-[#5d582a]">Category:</strong>
-        {{ meal.strCategory || "None" }}
+  <div class="relative">
+    <img
+      src="../images/bg3.webp"
+      alt="bg"
+      class="absolute top-0 w-[100vw] h-full opacity-15 z-[-1]"
+    />
+    <div class="flex flex-col items-center justify-center max-w-[50%] mx-auto">
+      <h1
+        class="text-8xl text-center calligraphy-paragraph font-bold text-[#5d582a] p-8"
+      >
+        {{ meal.strMeal }}
+      </h1>
+      <img
+        :src="meal.strMealThumb"
+        :alt="meal.strMeal"
+        class="w-[30rem] border-2 border-[#5d582a]"
+      />
+      <div
+        class="grid text-[#5d582a] grid-cols-1 mt-2 sm:grid-cols-3 text-lg py-2 justify-evenly text-center"
+      >
+        <div>
+          <strong class="font-bold text-[#5d582a]">Category:</strong>
+          {{ meal.strCategory || "None" }}
+        </div>
+        <div>
+          <strong class="font-bold text-[#5d582a]">Area:</strong>
+          {{ meal.strArea || "None" }}
+        </div>
+        <div>
+          <strong class="font-bold text-[#5d582a]">Tags:</strong>
+          {{ meal.strTags || "None" }}
+        </div>
       </div>
-      <div>
-        <strong class="font-bold text-[#5d582a]">Area:</strong>
-        {{ meal.strArea || "None" }}
-      </div>
-      <div>
-        <strong class="font-bold text-[#5d582a]">Tags:</strong>
-        {{ meal.strTags || "None" }}
-      </div>
-    </div>
 
-    <div class="flex text-[#5d582a] w-full">
-      <div class="flex flex-col w-[50%] justify-center items-center">
-        <h2 class="text-2xl just font-semibold mb-3">Ingredients</h2>
-        <ul>
-          <template v-for="ind in 20" :key="ind">
-            <li v-if="meal[`strIngredient${ind}`]">
-              {{ ind }}. {{ meal[`strIngredient${ind}`] }}
-            </li></template
-          >
-        </ul>
+      <div class="flex text-[#5d582a] w-full">
+        <div class="flex flex-col w-[50%] justify-center items-center">
+          <h2 class="text-2xl just font-semibold mb-3">Ingredients</h2>
+          <ul>
+            <template v-for="ind in 20" :key="ind">
+              <li v-if="meal[`strIngredient${ind}`]">
+                {{ ind }}. {{ meal[`strIngredient${ind}`] }}
+              </li></template
+            >
+          </ul>
+        </div>
+        <div class="flex flex-col w-[50%] justify-center items-center">
+          <h2 class="text-2xl font-semibold mb-3">Measurements</h2>
+          <ul>
+            <template v-for="ind in 20" :key="ind">
+              <li v-if="meal[`strMeasure${ind}`] != 0">
+                {{ ind }}. {{ meal[`strMeasure${ind}`] }}
+              </li></template
+            >
+          </ul>
+        </div>
       </div>
-      <div class="flex flex-col w-[50%] justify-center items-center">
-        <h2 class="text-2xl font-semibold mb-3">Measurements</h2>
-        <ul>
-          <template v-for="ind in 20" :key="ind">
-            <li v-if="meal[`strMeasure${ind}`] != 0">
-              {{ ind }}. {{ meal[`strMeasure${ind}`] }}
-            </li></template
-          >
-        </ul>
+      <div class="mt-8 mb-10">
+        <YoutubeButton :href="meal.strYoutube" />
       </div>
-    </div>
-    <div class="mt-8 mb-10">
-      <YoutubeButton :href="meal.strYoutube" />
     </div>
   </div>
 </template>
